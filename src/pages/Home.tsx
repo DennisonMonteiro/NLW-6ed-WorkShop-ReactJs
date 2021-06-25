a79491f51a4517aa94572a4e2d3fdbef722cdc85
Dennison Monteiro <dennison.cp2@gmail.com>
2021-06-25 16:31:40 -0300

    Aula 3: Fluxo de salas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,13 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 import '../styles/auth.scss'
 import { useAuth } from '../hooks/useAuth'
+import { FormEvent, useState } from 'react'
+import { database } from '../services/firebase'
 
 export function Home() {
     const history = useHistory()
     const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom() { 
         if (!user) {
@@ -18,10 +21,25 @@ export function Home() {
         history.push('/rooms/new')
     }
 
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+        if (!roomRef.exists()) {
+            alert('Room does not exists.')
+            return;
+        }
+        history.push(`/rooms/${roomCode}`)
+    }
+
     return(
         <div id="page-auth">
             <aside>
-                <img src={illustrationImg} alt="Ilustraão simbolizando perguntas e respostas" />
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvdas da sua audiência em tempo-real</p>
             </aside>
@@ -35,10 +53,12 @@ export function Home() {
                     <div className="separator">
                         ou entre em uma sala
                     </div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={e => setRoomCode(e.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
